2a4fea1c288db9e36814a4c14479697c7ec0c603
ThurrHenrick062 <arthurhenrick062@gmail.com>
2026-05-14 22:32:25 -0300

    primeiro commit

---
+++ backend/src/models/Member.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose"; // ← remova o "mongo" que não está sendo usado
+
+const memberSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Nome é obrigatório"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email é obrigatório"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    position: {
+      type: Number,
+      required: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("Member", memberSchema);
